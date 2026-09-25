8a9deaf1f31d9501b16cc6d32ca3c557eea630e5
Jose Beltran <YOUR-ACTUAL-GITHUB-EMAIL>
2026-09-25 15:15:22 -0700

    Create Spring Boot Lab 2 application

---
+++ src/test/java/com/gcu/lab2api/Lab2ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcu.lab2api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
